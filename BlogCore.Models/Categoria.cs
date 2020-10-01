eb48eb85d77b0026514c2883a575e12bb857b015
movit 3 <luis.quira@movit.com.co>
2020-10-01 17:20:23 -0500

    Configuración  Repository

---
+++ BlogCore.Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogCore.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Ingrese un nombre para la categoria")]
+        [Display(Name ="Nombre Categoría")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Orden de Visualización")]
+        public string Orden { get; set; }
+    }
+}
